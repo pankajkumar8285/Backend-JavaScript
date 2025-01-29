d76a30148338bb2f8e9a2167b4f84d708b44ca45
pankajkumar8285 <168230102+pankajkumar8285@users.noreply.github.com>
2025-01-29 20:47:14 +0530

    completed

--- SERVER/Admin/routes/jobRoutes.js
+++ SERVER/Admin/routes/jobRoutes.js
@@ -5,7 +5,8 @@ const adminMiddlewares = require("../middleware/adminMiddleware");
 
 const jobControllers = require("../controller/jobController");
 jobRouter.post('/create-job',adminMiddlewares.isAdmin,jobControllers.createJob);
-
+jobRouter.put("/update-job/:id",jobControllers.updateJob);
+jobRouter.get("/delete-job/:id",jobControllers.deleteJob);
 
 
 module.exports = jobRouter;
