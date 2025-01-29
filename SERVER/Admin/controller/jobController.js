8beab3bb98b2509dc3a35e0710e0bb42f9510f05
pankajkumar8285 <168230102+pankajkumar8285@users.noreply.github.com>
2025-01-29 20:43:04 +0530

    full completed

--- SERVER/Admin/controller/jobController.js
+++ SERVER/Admin/controller/jobController.js
@@ -16,3 +16,39 @@ exports.createJob = async(req,res) => {
         res.status(500).json({code: 500, message: "Internal Server Error", Error: err})
     }
 }
+
+
+exports.updateJob = async(req,res) => {
+    try {
+        const id =req.params.id;
+        const {title,posts,location,qualification} = req.body;
+        const filter = {
+            _id: id
+        }
+        const data = {
+            title: title,
+            posts:posts,
+            location:location,
+            qualification:qualification
+        }
+        await jobModels.findByIdAndUpdate(filter,data);
+        res.status(200).json({code: 200, message: "Job updated Successfully"})
+
+    }catch (err) {
+        res.status(500).json({code: 500, message: "Internal Server Error", Error: err})
+    }
+}
+
+exports.deleteJob = async(req, res) => {
+    try {
+        const id = id;
+        const filter = {
+            _id : id
+        };
+        await jobModels.findByIdAndDelete(filter);
+        res.status(200).json({code: 200, message: "Job deleted successfully"})
+
+    }catch (err) {
+        res.status(500).json({code: 500, message: "Internal Server Error", Error: err})
+    }
+}
